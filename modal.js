d5aa4336f6220a434afc2b8f188d9f00910bda0a
Nate Micinski <nmicinski@gmail.com>
2019-06-13 16:51:21 -0700

    button controls visibility of modal div

--- modal.js
+++ modal.js
@@ -1,1 +1,9 @@
-console.log("hello!");
+document.getElementById("modal-btn").onclick = () => {
+    let modalDiv = document.getElementById("modal");
+
+    if (modalDiv.getAttribute("style") === "visibility: hidden") {
+        modalDiv.setAttribute("style", "visibility: visible");
+    } else {
+        modalDiv.setAttribute("style", "visibility: hidden");
+    }
+}
