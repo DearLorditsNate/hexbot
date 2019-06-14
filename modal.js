639c5e5c8fb6be2d13d90e9c8ed70b12627fbf37
Nate Micinski <nmicinski@gmail.com>
2019-06-13 18:00:22 -0700

    can now click anywhere but modal to dismiss it

--- modal.js
+++ modal.js
@@ -1,14 +1,25 @@
-document.getElementById("modal-btn").onclick = () => {
-    let modalDiv = document.getElementById("modal");
-    let container = document.getElementById("container");
+let modalDiv = document.getElementById("modal");
+let container = document.getElementById("container");
 
-    if (modalDiv.classList.contains("modal-hide")) {
-        modalDiv.classList.remove("modal-hide");
-        modalDiv.classList.add("modal-show");
-        container.classList.add("is-blurred");
-    } else {
+document.getElementById("modal-btn").onclick = () => {
+    modalDiv.classList.remove("modal-hide");
+    modalDiv.classList.add("modal-show");
+    container.classList.add("is-blurred");
+    container.setAttribute("data-open-status", "isOpen");
+}
+
+document.getElementById("close-btn").onclick = () => {
+    modalDiv.classList.remove("modal-show");
+    modalDiv.classList.add("modal-hide");
+    container.classList.remove("is-blurred");
+    container.setAttribute("data-open-status", "isClosed");
+}
+
+window.onclick = function(e) {
+    if (e.target == modalDiv) {
         modalDiv.classList.remove("modal-show");
         modalDiv.classList.add("modal-hide");
         container.classList.remove("is-blurred");
+        container.setAttribute("data-open-status", "isClosed");
     }
 }
