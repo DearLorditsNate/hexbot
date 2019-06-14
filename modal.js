711f50712dc4321baec3198e8f3caabc52126769
Nate Micinski <nmicinski@gmail.com>
2019-06-13 17:14:08 -0700

    fixed so modal doesn't blur also

--- modal.js
+++ modal.js
@@ -1,13 +1,14 @@
 document.getElementById("modal-btn").onclick = () => {
     let modalDiv = document.getElementById("modal");
+    let container = document.getElementById("container");
 
     if (modalDiv.classList.contains("modal-hide")) {
         modalDiv.classList.remove("modal-hide");
         modalDiv.classList.add("modal-show");
-        document.body.classList.add("is-blurred");
+        container.classList.add("is-blurred");
     } else {
         modalDiv.classList.remove("modal-show");
         modalDiv.classList.add("modal-hide");
-        document.body.classList.remove("is-blurred");
+        container.classList.remove("is-blurred");
     }
 }
